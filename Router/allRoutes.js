9c949fe7573152ae4d76bfb2cedeaaf21c7b76a3
BakStudi <drgkill@hotmail.com>
2026-02-22 23:16:22 +0100

    Added gallery page + style adjustment

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,8 +2,8 @@ import Route from "./Route.js";
 
 // Définition des routes
 export const allRoutes = [
-	new Route("/", "Accueil", "/pages/home.html")
-	// new Route("/Galerie", "Galerie", "/pages/galerie.html", "/js/galerie.js")
+	new Route("/", "Accueil", "/pages/home.html"),
+	new Route("/galerie", "Galerie", "/pages/galerie.html", "/js/galerie.js")
 ];
 
 // Le titre s'affiche comme ceci : Route.titre - websiteName
