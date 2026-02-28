72f4d74bbdfcc2495904d27704137d55c743c76d
BakStudi <drgkill@hotmail.com>
2026-02-28 17:09:43 +0100

    Add delete confirmation modal + breakpoints management

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,7 @@ import Route from "./Route.js";
 export const allRoutes = [
 	new Route("/", "Accueil", "/pages/home.html"),
 	new Route("/galerie", "Galerie", "/pages/galerie.html", "/js/galerie.js"),
-	new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+	new Route("/signup", "Inscription", "/pages/auth/signup.html", "/js/auth/signup.js"),
 	new Route("/signin", "Connexion", "/pages/auth/signin.html"),
 	new Route("/account", "Mon compte", "/pages/auth/account.html"),
 	new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
